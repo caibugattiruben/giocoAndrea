bc27a7052683c22478ab2e0bb8452004b24f047b
ruben <caibugatti.ruben@franchettisalviani.net>
2025-05-12 13:21:13 +0200

    funzia

--- GiocoAndrea/GiocoAndrea/Program.cs
+++ GiocoAndrea/GiocoAndrea/Program.cs
@@ -6,52 +6,48 @@
     {
         string carta = valori[rdn.Next(0, 14)];
         carta += ","+seme[rdn.Next(0, 4)];
-        mano.Add(carta);
+        if (!mano.Contains(carta))
+        {
+            mano.Add(carta);
+        }
         carta = "";
-    }
-    foreach(string s in mano)
-    {
-        Console.WriteLine(s);
     }
 }
 bool indovina(List<string> mano,List<string> valori,List<string> seme)
 {
     string[] mano1=mano.ToArray();
-    foreach(string s in mano1)
-    {
-        Console.WriteLine(s);
-    }
     Console.WriteLine("Scegli una carta, le carte sono "+mano1.Length);
     int risposta = int.Parse(Console.ReadLine());
     risposta--;
-    string max = mano1[0];
-    Console.WriteLine(max);
-    string max1=mano1[2];
-    Console.WriteLine(max1);
+    string[] carta = mano1[0].Split(',');
+    string max = carta[0];
+    string max1 = carta[1];
     foreach (string s in mano1)
     {
-        string[] carta = s.Split(',');
-        if (valori.IndexOf(carta[0]) > valori.IndexOf(max))
+        if(s.Length > 0)
         {
-            return true;
+            string[] carta1 = s.Split(',');
+            string maxC = carta1[0];
+            string max1S = carta1[1];
+            if(valori.IndexOf(maxC)> valori.IndexOf(max))
+            {
+                max = maxC;
+                max1 = max1S;
+            }
+            else if(valori.IndexOf(maxC) == valori.IndexOf(max) && seme.IndexOf(max1S) > seme.IndexOf(max1))
+            {
+                max1 = max1S;
+            }
         }
-        else if (valori.IndexOf(carta[0]) == valori.IndexOf(max)&& seme.IndexOf(carta[0]) > seme.IndexOf(max1))
-        {
-            return true;
-        }
-        else if(valori.IndexOf(carta[0]) == valori.IndexOf(max) && seme.IndexOf(carta[0]) < seme.IndexOf(max1))
-        {
-            return false;
-        }
-        else if(valori.IndexOf(carta[0]) < valori.IndexOf(max))
-        {
-            return false;
-        }
+    }
+    if (mano1[risposta] == max + "," + max1)
+    {
+        return true;
     }
     return false;
 }
-List<string> valori = new List<string>(["1", "2","3","4","5","6","7","8","9","10","J","Q","K","A"]);
-List<string> seme = new List<string>(["P", "F", "Q", "C"]);
+List<string> valori = new List<string>(["1", "2","3","4","5","6","7","8","9","y","J","Q","K","A"]);
+List<string> seme = new List<string>(["Picche", "Fiori", "Quadri", "Cuori"]);
 List<string> mano = new List<string>();
 bool chiudi = true;
 int tentativi = 1;
@@ -61,6 +57,19 @@ while (chiudi!=false)
     
     generoCarta(tentativi,valori, seme, mano);
     bool indovinato = indovina(mano,valori,seme);
+    Console.WriteLine("Le carte erano:");
+    foreach(string s in mano)
+    {
+        if (s == (mano.Count-1).ToString())
+        {
+            Console.Write(s);
+        }
+        else
+        {
+            Console.Write(s + " | ");
+        }
+    }
+    Console.WriteLine();
     if (indovinato == true)
     {
         Console.WriteLine("Hai indovinato!!!");
